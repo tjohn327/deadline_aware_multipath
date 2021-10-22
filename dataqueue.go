2152315c32e612842aae1fbf9cadcf09a3c4443e
Tony John <tonyjanugrah@gmail.com>
2021-10-23 01:09:57 +0200

    dataqueue implemented

--- dataqueue.go
+++ dataqueue.go
@@ -3,47 +3,90 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
+)
+
+const DefaultDeadline = time.Duration(50 * time.Millisecond)
+
+type DataQueueType int
+
+const (
+	Send DataQueueType = iota
+	UnAck
+	Receive
 )
 
 type DataQueue struct {
+	queueType   DataQueueType
 	headBlockID int
 	tailBlockID int
 	len         int
-	mutex       sync.Mutex
+	deadline    *time.Duration
 	blocks      []*DataBlock
-	ingressChan chan *DataBlock
+	ingressChan chan *DataFragment
 	egressChan  chan *DataBlock
-	ackChan     chan *DataFragment
-	isRemote    bool
+	mutex       sync.Mutex
 }
 
-func NewDataQueue(ingressChan chan *DataBlock, egressChan chan *DataBlock,
-	ackChan chan *DataFragment, isRemote bool) *DataQueue {
+func NewDataQueue(t DataQueueType, ingressChan chan *DataFragment, egressChan chan *DataBlock,
+	deadline *time.Duration) *DataQueue {
+
 	if ingressChan == nil {
-		ingressChan = make(chan *DataBlock, 10)
+		ingressChan = make(chan *DataFragment, 10)
 	}
-	if egressChan == nil {
-		egressChan = make(chan *DataBlock, 10)
-	}
-	if ackChan == nil {
-		ackChan = make(chan *DataFragment, 10)
-	}
-	return &DataQueue{
+
+	dq := &DataQueue{
+		queueType:   t,
 		headBlockID: 0,
 		tailBlockID: 0,
 		len:         0,
+		deadline:    deadline,
 		blocks:      make([]*DataBlock, 0),
 		ingressChan: ingressChan,
 		egressChan:  egressChan,
-		ackChan:     ackChan,
-		isRemote:    isRemote,
+	}
+
+	if t == Receive {
+		dq.runReceive()
+	}
+	if t == UnAck {
+		dq.runAck()
+	}
+	return dq
+}
+
+func (dq *DataQueue) runReceive() {
+	go func() {
+		for {
+			f := <-dq.ingressChan
+			dq.InsertFragment(f)
+		}
+	}()
+}
+
+func (dq *DataQueue) runAck() {
+	go func() {
+		for {
+			f := <-dq.ingressChan
+			dq.ProcessACK(f)
+		}
+	}()
+}
+
+func (dq *DataQueue) retransmit(blockID int) {
+	deadline := (*dq.deadline)
+	timer := time.NewTimer(deadline)
+	<-timer.C
+	out := dq.getDataBlockbyID(blockID)
+	if out != nil {
+		if out.unackedCount > out.parityCount {
+			dq.egressChan <- out
+		}
 	}
 }
 
 func (dq *DataQueue) InsertBlock(db *DataBlock) error {
-	dq.mutex.Lock()
-	defer dq.mutex.Unlock()
-	_, in := dq.IsDataBlockIn(db.blockID)
+	_, in := dq.isDataBlockIn(db.blockID)
 	if !in {
 		dq.blocks = append(dq.blocks, db)
 		dq.len++
@@ -53,26 +96,68 @@ func (dq *DataQueue) InsertBlock(db *DataBlock) error {
 		} else {
 			dq.tailBlockID = db.blockID
 		}
+		if dq.queueType == UnAck {
+			go dq.retransmit(db.blockID)
+		}
 		return nil
 	}
 	return fmt.Errorf("blockid: %d already exists", db.blockID)
 }
 
 func (dq *DataQueue) InsertFragment(f *DataFragment) {
-	if f.blockID == dq.headBlockID {
-		dq.blocks[0].InsertFragment(f)
-	} else {
-		i, in := dq.IsDataBlockIn(f.blockID)
-		if in {
-			dq.blocks[i].InsertFragment(f)
-		} else {
-			db := NewDataBlockFromFragment(f)
-			dq.InsertBlock(db)
+	dq.mutex.Lock()
+	defer dq.mutex.Unlock()
+	i, in := dq.isDataBlockIn(f.blockID)
+	if in {
+		dq.blocks[i].InsertFragment(f)
+		if dq.blocks[i].canDecode && dq.queueType == Receive && dq.egressChan != nil {
+			out := dq.getDataBlock(i)
+			if out != nil {
+				dq.egressChan <- out
+			}
 		}
+	} else {
+		db := NewDataBlockFromFragment(f)
+		dq.InsertBlock(db)
 	}
 }
 
-func (dq *DataQueue) IsDataBlockIn(blockID int) (int, bool) {
+func (dq *DataQueue) GetDataBlock() (*DataBlock, error) {
+	dq.mutex.Lock()
+	defer dq.mutex.Unlock()
+	if dq.len == 0 {
+		err := fmt.Errorf("queue empty")
+		return nil, err
+	}
+	out := dq.getDataBlock(0)
+	return out, nil
+}
+
+func (dq *DataQueue) getDataBlockbyID(blockID int) *DataBlock {
+	i, in := dq.isDataBlockIn(blockID)
+	if !in {
+		return nil
+	}
+	out := dq.getDataBlock(i)
+	return out
+}
+
+func (dq *DataQueue) getDataBlock(i int) *DataBlock {
+	if i >= dq.len || dq.len == 0 {
+		return nil
+	}
+	out := dq.blocks[i]
+	dq.blocks = append(dq.blocks[:i], dq.blocks[i+1:]...)
+	dq.blocks = dq.blocks[i:dq.len]
+	dq.len--
+	if dq.len > 0 {
+		dq.headBlockID = dq.blocks[0].blockID
+		dq.tailBlockID = dq.blocks[len(dq.blocks)-1].blockID
+	}
+	return out
+}
+
+func (dq *DataQueue) isDataBlockIn(blockID int) (int, bool) {
 	if len(dq.blocks) == 0 {
 		return 0, false
 	}
@@ -82,4 +167,25 @@ func (dq *DataQueue) IsDataBlockIn(blockID int) (int, bool) {
 		}
 	}
 	return 0, false
+}
+
+func (dq *DataQueue) ProcessACK(f *DataFragment) {
+	i, in := dq.isDataBlockIn(f.blockID)
+	if in {
+		dq.blocks[i].AcknowledgeFragment(f)
+	}
+}
+
+func (dq *DataQueue) IsDataBlockIn(blockID int) bool {
+	dq.mutex.Lock()
+	defer dq.mutex.Unlock()
+	if len(dq.blocks) == 0 {
+		return false
+	}
+	for i := range dq.blocks {
+		if dq.blocks[i].blockID == blockID {
+			return true
+		}
+	}
+	return false
 }
---
+++ dataqueue_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"crypto/md5"
+	"io/ioutil"
+	"testing"
+	"time"
+)
+
+func TestDataQueue(t *testing.T) {
+	in, err := ioutil.ReadFile("testdata/test_image.jpg")
+	if err != nil {
+		t.Error("error reading test image")
+	}
+	inHash := md5.Sum(in)
+	splitData, err := Split(in, 999)
+	if err != nil {
+		t.Error("error splitting data")
+	}
+	parityCount := 3
+	reedSolomon, err := NewReedSolomon(splitData.fragmentCount, parityCount)
+	if err != nil {
+		t.Error("error creating encoder:", err)
+		return
+	}
+
+	encodedData, err := reedSolomon.Encode(splitData, parityCount)
+	if err != nil {
+		t.Error("error encoding:", err)
+		return
+	}
+
+	db := NewDataBlockFromEncodedData(encodedData, 1)
+
+	receiveChan := make(chan *DataFragment, 2)
+	ackChan := make(chan *DataFragment, 2)
+	retransmitChan := make(chan *DataBlock, 2)
+	egressChan := make(chan *DataBlock, 2)
+
+	deadline := time.Duration(time.Second)
+	unackQueue := NewDataQueue(UnAck, ackChan, retransmitChan, &deadline)
+	NewDataQueue(Receive, receiveChan, egressChan, nil) //receive queue
+	unackQueue.InsertBlock(db)
+
+	for i, v := range db.fragments {
+		receiveChan <- v
+		ackChan <- v
+		if i == 185 {
+			break
+		}
+	}
+
+	db_ret := <-retransmitChan
+	for _, v := range db_ret.fragments {
+		if !v.acked {
+			receiveChan <- v
+		}
+	}
+
+	db_out := <-egressChan
+
+	encodedDataOut, err := db_out.GetEncodedData()
+	if err != nil {
+		t.Error("error retreiving encoded data:", err)
+		return
+	}
+	out, err := reedSolomon.Decode(encodedDataOut)
+	if err != nil {
+		t.Error("error decoding:", err)
+		return
+	}
+	outHash := md5.Sum(out)
+	if inHash != outHash {
+		t.Error("decoding failed, hash mismatch")
+	}
+}
